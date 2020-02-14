72680b8eb7bcd889b3b4a12e611c366bde4d3f54
bentatsuoka <41030028+bentatsuoka@users.noreply.github.com>
2020-02-14 13:01:56 -0500

    Update and rename Sorting_hw2.java to SortingAlgorithms.java

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Sorting_hw2 {
+public class SortingAlgorithms {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		sc.useDelimiter("\n");
